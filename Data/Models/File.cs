7d3534cf8a4e235749abab2ad910dc467b93d99c
Xcdter <tigrasha999@ya.ru>
2024-10-08 10:33:24 +0300

    Initial check in

---
+++ Data/Models/File.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YouToDo.Data.Models
+{
+    public class File
+    {
+        public int UserId { get; set; }
+
+        public char FileName { get; set; }
+
+        public char Type { get; set; }
+
+        public char Password { get; set; }
+    }
+}
